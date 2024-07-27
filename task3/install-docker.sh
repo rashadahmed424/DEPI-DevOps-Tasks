e5b409714a7c109cbd055f957d313dc2e0380b37
rashadahmed424 <99238868+rashadahmed424@users.noreply.github.com>
2024-07-27 04:01:30 +0300

    Update install-docker.sh

--- task3/install-docker.sh
+++ task3/install-docker.sh
@@ -10,8 +10,8 @@ if ! command -v docker; then
 
 	# Add the repository to Apt sources:
 	echo \
-	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/li	nux/ubuntu \
-  	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin

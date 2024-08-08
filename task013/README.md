285e3644c0b7084ce71114e70f8d04288ff5c694
root <root@HP-PROBOOK>
2024-08-09 00:37:40 +0300

    jenkins piplein to build, testand deploy java app

---
+++ task013/README.md
@@ -0,0 +1,3 @@
+### this project is to build spring-pet-clinic app with jenkins and push final image to docker hub rpo.
+### build tool for this java app is maven 
+ 
---
+++ task013/script.groovy
@@ -0,0 +1,20 @@
+def build_jar(){
+    sh "mvn install"
+}
+
+def test(){
+    sh "mvn test"
+}
+
+
+
+def build_image(){
+    withCredentials([ usernamePassword(credentialsId: 'dockerhub-conn', usernameVariable: 'USER', passwordVariable: 'PASS')])
+        {
+            sh "docker build -t rashadahmed424/spring-pet-app:BUILD_NUMBER ."
+            sh "echo $PASS | docker login -u ${USER} --password-stdin"
+            sh "docker push rashadahmed424/spring-pet-app:v2.0"
+        }
+
+}
+return this

2024acf69604e0f55beff898c50bd80ca389cbda
rashadahmed424 <99238868+rashadahmed424@users.noreply.github.com>
2024-08-16 15:02:34 +0300

    Update install-docker.sh

--- task3/install-docker.sh
+++ task3/install-docker.sh
@@ -3,7 +3,7 @@ if ! command -v docker; then
 
 	# Add Docker's official GPG key:
 	sudo apt-get update
-	sudo apt-get install ca-certificates curl
+	sudo apt-get install -y ca-certificates curl
 	sudo install -m 0755 -d /etc/apt/keyrings
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,7 +14,7 @@ if ! command -v docker; then
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 else 
 	echo "Docker Already Installed"
 fi	
